f9e50b1f066e6b03701f7476f05505494c975835
Xuanye Wong <xuanye.wan@gmail.com>
2021-03-18 09:37:55 +0800

    🎨 修改mysql依赖库

--- bin/code-gear.js
+++ bin/code-gear.js
@@ -1,13 +1,13 @@
 #! /usr/bin/env node
 
 const program = require('commander'),
-  chalk = require('chalk'),
-  codeGear = require('../lib/index');
+    chalk = require('chalk'),
+    codeGear = require('../lib/index');
 
 program
-  .version('1.0.0')  
-  .option('-c, --config [config]', '配置文件名称','code-gear.config.js')
-  .description('生成数据库实体类')
-  .parse(process.argv);
+    .version('1.0.1')
+    .option('-c, --config [config]', '配置文件名称', 'code-gear.config.js')
+    .description('生成数据库实体类')
+    .parse(process.argv);
 
 codeGear.generate(program.config);
